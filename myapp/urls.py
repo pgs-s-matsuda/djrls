e8577320596dc8ed8be1a641c382487e6704d230
s-matsuda <s-matsuda@pgsystem.co.jp>
2021-06-21 10:15:18 +0900

    app

--- myapp/urls.py
+++ myapp/urls.py
@@ -5,6 +5,9 @@ from myapp import views
 app_name = 'myapp'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
-    path('<uuid:pk>/', views.CustomerUpdateView.as_view(), name='update'),
-    path('create/', views.CustomerCreateView.as_view(), name='create'),
+    path('customer/', views.CustomerIndexView.as_view(), name='customerindex'),
+    path('customer/<uuid:pk>/', views.CustomerUpdateView.as_view(), name='customerupdate'),
+    path('customer/create/', views.CustomerCreateView.as_view(), name='customercreate'),
+    path('app1/', views.App1IndexView.as_view(), name='app1index'),
+    path('app2/', views.App2IndexView.as_view(), name='app2index'),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -2,12 +2,14 @@ from django.shortcuts import render
 from django.views import generic
 from django.contrib.auth.mixins import LoginRequiredMixin
 
-from myapp.models import Customer
+from myapp.models import Customer, Tenant
 from myapp.forms import CustomerForm
 
-# Create your views here.
-class IndexView(LoginRequiredMixin, generic.ListView):
+class IndexView(LoginRequiredMixin, generic.TemplateView):
     template_name = 'myapp/index.html'
+
+class CustomerIndexView(LoginRequiredMixin, generic.ListView):
+    template_name = 'customer/index.html'
     context_object_name = 'customer_list'
 
     def get_queryset(self):
@@ -16,12 +18,17 @@ class IndexView(LoginRequiredMixin, generic.ListView):
 class CustomerUpdateView(LoginRequiredMixin, generic.UpdateView):
     model = Customer
     form_class = CustomerForm
-    template_name = "myapp/detail.html"
+    template_name = "customer/detail.html"
     success_url = "/"
 
 class CustomerCreateView(LoginRequiredMixin, generic.CreateView):
     model = Customer
     form_class = CustomerForm
-    template_name = "myapp/detail.html"
+    template_name = "customer/detail.html"
     success_url = "/"
 
+class App1IndexView(LoginRequiredMixin, generic.TemplateView):
+    template_name = 'app1/index.html'
+
+class App2IndexView(LoginRequiredMixin, generic.TemplateView):
+    template_name = 'app2/index.html'
